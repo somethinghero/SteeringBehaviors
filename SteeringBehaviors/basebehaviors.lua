2c2a49130689c2781fab669b4243e8c766ecdd9e
somethinghero <somethinghero@163.com>
2018-03-19 10:43:41 +0800

    seek implement

---
+++ SteeringBehaviors/basebehaviors.lua
@@ -0,0 +1,33 @@
+local M = class("basebehaviors")
+
+function M:ctor()
+
+end
+
+function M:truncateVec2(value, max)
+	if value.x > max.x then
+		value.x = max.x
+	end
+	if value.y > max.y then
+		value.y = max.y
+	end
+end
+
+function M:getDesiredVelocity(start_pos, end_pos)
+	local desired_velocity = cc.p(end_pos.x - start_pos.x, end_pos.y - start_pos.y)
+	desired_velocity = cc.pNormalize(desired_velocity)
+	return desired_velocity
+end
+
+function M:getSteering(desired_velocity)
+	local steering = cc.p(desired_velocity.x - self.velocity.x, desired_velocity.y - self.velocity.y)
+
+	return steering
+end
+
+--child class should implement this func
+function M:update(dt)
+
+end
+
+return M
---
+++ SteeringBehaviors/seek.lua
@@ -0,0 +1,54 @@
+local base = require("SteeringBehaviors.basebehaviors")
+local M = class("seek", base)
+
+function M:ctor()
+	self.max_velocity = cc.p(1, 2) --最大速度
+	self.max_force = cc.p(2, 2) --最大力量
+	self.mass = 1 --质量，暂时不用
+	self.velocity = cc.p(-2, 0) --当前速度
+	
+	self._target = nil --目标
+	self._seeker = nil --搜索者
+end
+
+function M:setTarget(t)
+	self._target = t
+end
+
+function M:setSeeker(s)
+	self._seeker = s
+end
+
+function M:update(dt)
+	--desired_velocity = normalize(target - position) * max_velocity
+	--steering = desired_velocity - velocity
+	--steering = truncate (steering, max_force)
+	--steering = steering / mass
+	--velocity = truncate (velocity + steering , max_speed)
+	--position = position + velocity
+
+	local target_x, target_y = self._target:getPosition()
+	local seek_x, seek_y = self._seeker:getPosition()
+
+	--理想移动
+	local desired_velocity = self:getDesiredVelocity(cc.p(seek_x, seek_y), cc.p(target_x, target_y))
+	desired_velocity.x = desired_velocity.x * self.max_velocity.x
+	desired_velocity.y = desired_velocity.y * self.max_velocity.y
+
+	--加上转向
+	local steering = self:getSteering(desired_velocity)
+	self:truncateVec2(steering, self.max_force)
+	steering.x = steering.x / self.mass
+	steering.y = steering.y / self.mass
+	self.velocity.x = self.velocity.x + steering.x
+	self.velocity.y = self.velocity.y + steering.y
+	self:truncateVec2(self.velocity, self.max_velocity)
+
+	local positionx, positiony = self._seeker:getPosition()
+	positionx = positionx + self.velocity.x
+	positiony = positiony + self.velocity.y
+	self._seeker:setPosition(positionx, positiony)
+
+end
+
+return M
